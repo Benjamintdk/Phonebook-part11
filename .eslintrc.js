4b21aa6f4bedc667ee28b008079714852e014db1
Benjamintdk <bc.chew.hong.ee@gmail.com>
2022-01-08 15:29:35 +0800

    installed linting dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,24 @@
 module.exports = {
     "env": {
-      "node": true,
-      "browser": true,
-      "es6": true,
-      "jest/globals": true
+        "node": true,
+        "browser": true,
+        "es6": true,
+        "jest/globals": true
     },
     "extends": [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "plugin:cypress/recommended"
+        "eslint:recommended",
+        "plugin:react/recommended"
     ],
     "parserOptions": {
-      "ecmaFeatures": {
-        "jsx": true
-      },
-      "ecmaVersion": 2018,
-      "sourceType": "module"
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13,
+        "sourceType": "module"
     },
     "plugins": [
-      "react", "jest"
+        "react"
     ],
     "rules": {
-      "indent": [
-        "error",
-          2
-        ],
-      "linebreak-style": [
-        "error",
-        "unix"
-      ],
-      "quotes": [
-        "error",
-        "single"
-      ],
-      "semi": [
-        "error",
-        "never"
-      ],
-      "eqeqeq": "error",
-      "no-trailing-spaces": "error",
-      "object-curly-spacing": [
-        "error", "always"
-      ],
-      "arrow-spacing": [
-        "error", { "before": true, "after": true }
-      ],
-      "no-console": "error",
-      "react/prop-types": 0
     }
-  }
-  
+};
